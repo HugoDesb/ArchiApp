edc3aee7e850a01c383662eebd4f2b48eb59aae2
HugoDesb <hugo.desbiolles@gmail.com>
2018-04-10 14:46:06 +0200

    Test avec injection de dépendance

---
+++ src/main/java/com/polytech/web/FeedController.java
@@ -0,0 +1,26 @@
+package com.polytech.web;
+
+import com.polytech.services.FeedService;
+import com.polytech.services.PublicationService;
+import com.polytech.services.Story;
+
+import java.util.List;
+
+public class FeedController {
+
+    private PublicationService publicationService ;
+    private FeedService feedService;
+
+    public FeedController(PublicationService publicationService, FeedService feedService) {
+        this.publicationService = publicationService;
+        this.feedService = feedService;
+    }
+
+    public void post(String content) {
+        publicationService.share(new Story(content));
+    }
+
+    public List<Story> feed() {
+        return feedService.fetchAll();
+    }
+}
---
+++ src/test/java/PublicationTest.java
@@ -0,0 +1,40 @@
+
+
+import com.polytech.persistence.StoryRepository;
+import com.polytech.services.FeedService;
+import com.polytech.services.PublicationService;
+import com.polytech.services.Story;
+import com.polytech.web.FeedController;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PublicationTest {
+
+
+    private FeedController feedController;
+
+    @Before
+    public void setUp(){
+        StoryRepository storyRepository = new StoryRepository();
+        PublicationService publicationService=new PublicationService(storyRepository);
+        FeedService feedService=new FeedService(storyRepository);
+        feedController = new FeedController(publicationService, feedService);
+    }
+    @Test
+    public void should_post_story() {
+        //GIVEN
+        String story = "hi Info4";
+        
+        //WHEN
+        feedController.post(story);
+
+        //THEN
+        List<Story> postedStories = feedController.feed();
+        Assert.assertEquals(Arrays.asList(new Story("hi Info4")), postedStories);
+
+    }
+}
